8f587a65c6b01481b9ded859983612e150fb8dc5
YassineToumy <yassine.toumi@fsb.ucar.tn>
2026-03-13 20:46:30 +0100

    add low-level error capture to index.php for debugging

--- public/index.php
+++ public/index.php
@@ -5,6 +5,23 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+set_exception_handler(function(\Throwable $e) {
+    header('Content-Type: application/json');
+    http_response_code(500);
+    echo json_encode(['boot_error' => $e->getMessage(), 'file' => $e->getFile(), 'line' => $e->getLine()]);
+    exit;
+});
+register_shutdown_function(function() {
+    $error = error_get_last();
+    if ($error && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
+        header('Content-Type: application/json');
+        http_response_code(500);
+        echo json_encode(['fatal_error' => $error['message'], 'file' => $error['file'], 'line' => $error['line']]);
+    }
+});
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
